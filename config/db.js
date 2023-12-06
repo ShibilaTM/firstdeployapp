ae6e160a944ffa7b3afa773d6aac360c3b058f14
ShibilaTM <shibilaang@gmail.com>
2023-12-06 12:35:43 +0530

    first commit

--- config/db.js
+++ config/db.js
@@ -1,16 +1,27 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
 const MONGO_DB_URL = process.env.MONGO_DB_URL
 
-mongoose.connect(MONGO_DB_URL,{
-    dbName:'EmployeeDB',
-    useNewUrlParser: true, 
-    useUnifiedTopology: true
-})
+// mongoose.connect(MONGO_DB_URL,{
+//     dbName:'EmployeeDB',
+//     useNewUrlParser: true, 
+//     useUnifiedTopology: true
+// })
 
-.then(()=>{
-    console.log('MongoDB connection success')
-})
-.catch(error=>{
-    console.log('MongoDB coneection is not available'+error)
-})
+// .then(()=>{
+//     console.log('MongoDB connection success')
+// })
+// .catch(error=>{
+//     console.log('MongoDB coneection is not available'+error)
+// })
+
+const { MongoClient } = require('mongodb');
+
+const client = new MongoClient(MONGO_DB_URL , { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function connect() {
+  await client.connect();
+  console.log('Connected to MongoDB');
+}
+
+connect();
