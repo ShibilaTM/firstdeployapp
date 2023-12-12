74f1a9fe60b50664c93545d88118410982f4160b
ShibilaTM <shibilaang@gmail.com>
2023-12-13 00:01:21 +0530

    first commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ require('./config/db')
 const app = express(); 
 const PORT = process.env.PORT;
 const path = require('path');
-
+const { connect, client } = require('./config/db');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -27,7 +27,20 @@ app.use('/api/user', userRoute);
 const formRoute = require('./routes/formRoutes');
 app.use('/api/form', formRoute);
 
-app.listen(PORT, () => {
-  console.log(`server is listening on ${PORT}`);
+
+
+app.get("/items/:my_item", async (req, res) => {
+  let my_item = req.params.my_item;
+  let item = await client.db("EmployeeDB")
+              .collection("employeedatas")
+              .findOne({ my_item: my_item });
+
+  return res.json(item);
 });
 
+connect().then(() => {
+// Start the Express app after successful MongoDB connection
+app.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}`);
+});
+});
--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 // const mongoose = require('mongoose')
 
-const MONGO_DB_URL = process.env.MONGO_DB_URL
+
 
 // mongoose.connect(MONGO_DB_URL,{
 //     dbName:'EmployeeDB',
@@ -14,17 +14,45 @@ const MONGO_DB_URL = process.env.MONGO_DB_URL
 // .catch(error=>{
 //     console.log('MongoDB coneection is not available'+error)
 // })
+
+
+
+// --------------------------------------------------------------------
+// const MONGO_DB_URL = process.env.MONGO_DB_URL
+// const { MongoClient } = require('mongodb');
+
+// // Replace the connection string and options with your actual MongoDB connection details
+// const uri = MONGO_DB_URL
+
+// // New options: useNewUrlParser and useUnifiedTopology are no longer needed
+
+// const client = new MongoClient(uri, {
+//     serverSelectionTimeoutMS: 5000, // Increase the timeout value (in milliseconds)
+//     socketTimeoutMS: 45000, // Increase the socket timeout value
+//   });
+
+// async function connect() {
+//   try {
+//     await client.connect();
+//     console.log('Connected to MongoDB');
+//   } catch (error) {
+//     console.error('Error connecting to MongoDB:', error);
+//   }
+// }
+
+// connect();
+
+// -----------------------------------------------------------------------------
+
 const { MongoClient } = require('mongodb');
 
-// Replace the connection string and options with your actual MongoDB connection details
-const uri = MONGO_DB_URL
-
-// New options: useNewUrlParser and useUnifiedTopology are no longer needed
+const MONGO_DB_URL = process.env.MONGO_DB_URL;
+const uri = MONGO_DB_URL;
 
 const client = new MongoClient(uri, {
-    serverSelectionTimeoutMS: 5000, // Increase the timeout value (in milliseconds)
-    socketTimeoutMS: 45000, // Increase the socket timeout value
-  });
+  serverSelectionTimeoutMS: 5000,
+  socketTimeoutMS: 45000,
+});
 
 async function connect() {
   try {
@@ -35,5 +63,4 @@ async function connect() {
   }
 }
 
-connect();
-
+module.exports = { connect, client };
